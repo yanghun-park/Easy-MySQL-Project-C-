16b9c11fb9f8e1ffad039305daad694d5efa8322
yanghun-park <didgnsdl6784@naver.com>
2021-03-11 11:09:58 +0900

    First Upload

---
+++ SQL_Connect/main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SQL_Connect
+{
+    class main_class
+    {
+        public static void Main(String[] args)
+        {
+            // Simple applications that interact with C# and MySQL
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. INSERT");
+            Console.WriteLine("This is member table");
+
+            mysql userDB = new mysql(); // Create MySQL DB Generators
+            Console.Write("Your name : "); // Input Data
+            string name = Console.ReadLine();
+            Console.Write("Your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Your address : ");
+            string address = Console.ReadLine();
+
+            // Run SQL
+            Boolean check = userDB.Sql("Insert into member(name, age, address) Values('" + name + "'," + age + ",'" + address + "')");
+
+            if (check) // If check value is true
+                Console.WriteLine("Success");
+            else
+                Console.WriteLine("Fail");
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("2. UPDATE");
+            Console.WriteLine("This is member table");
+
+            Console.Write("Change name : ");
+            name = Console.ReadLine();
+            Console.Write("New name : ");
+            string new_name = Console.ReadLine();
+
+            // Run SQL
+            check = userDB.Sql("Update member Set name = '" + new_name + "' Where name = '" + name + "'");
+
+            if (check) // If check value is true
+                Console.WriteLine("Success");
+            else
+                Console.WriteLine("Fail");
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("3. SELECT (Single Value)");
+            Console.WriteLine("This is member table");
+
+            Console.Write("Your name : "); // If check value is true
+            name = Console.ReadLine();
+
+            // Run SQL
+            string result = userDB.Select_Sql("Select address From member Where name = '" + name + "'");
+
+            if (result != null) // If result value is not null
+                Console.WriteLine("Your Address are " + result);
+            else
+                Console.WriteLine("Fail");
+
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("4. SELECT (Multi Values)");
+            Console.WriteLine("This is member table");
+
+            Console.Write("Your name : "); // If check value is true
+            name = Console.ReadLine();
+
+            string[] result2 = new string[2];
+            string[] parameter = new string[2] { "address", "age" };
+
+            // Run SQL
+            result2 = userDB.Select_Sql("Select address, age From member Where name = '" + name + "'", parameter);
+
+            if (result2[0] != null) // If result2[0] value is not null
+            {
+                for (int i = 0; i < result2.Length; i++)
+                    Console.WriteLine(result2[i]);
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
---
+++ SQL_Connect/mysql.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+// ===============================================================
+// This class is designed to facilitate access to MySQL.
+//  * Last Update : 2021-03-11
+// ===============================================================
+
+namespace SQL_Connect
+{
+    class mysql
+    {
+        // DB Connection Information
+        private String Server_IP = "localhost"; // Server IP address
+        private String Server_Port = "3306"; // Server DB port
+        private String DB_Name = "test_db"; // Database name
+        private String DB_User = "root"; // Database user
+        private String DB_Password = "1234abcd"; // Database password
+
+        MySqlConnection connection;
+
+        // MySQL DB Generators
+        public mysql()
+        {
+            return;
+        }
+
+        public Boolean DBConnection()
+        {
+            string DB_Server_Connect = "Server=" + Server_IP + "; Port=" + Server_Port + "; Database=" + DB_Name + "; Uid=" + DB_User + "; Pwd=" + DB_Password;
+            try
+            {
+                connection = new MySqlConnection(DB_Server_Connect);
+                connection.Open();
+
+                Boolean tmp = connection.Ping(); // Check Connection
+
+                if (tmp) // If Connection to DB, return true
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public Boolean Sql(string SQL)
+        {
+            Boolean connection_result = DBConnection();
+
+            if (!connection_result) // If connection error
+            {
+                return false;
+            }
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(SQL, connection);
+                cmd.ExecuteNonQuery(); // Run SQL
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex) // If SQL process failed
+            {
+                connection.Close();
+                return false;
+            }
+        }
+
+        // Select SQL (Single value)
+        public string Select_Sql(string SQL)
+        {
+            Boolean connection_result = DBConnection();
+            string result = "";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(SQL, connection);
+                MySqlDataReader table = cmd.ExecuteReader();
+
+                while (table.Read())
+                {
+                    result = Convert.ToString(table[0]);
+                }
+                return result;
+            }
+            catch (Exception ex) // If SQL process failed
+            {
+                connection.Close();
+            }
+            return "0";
+        }
+
+        // Select SQL (Multi Values)
+        public string[] Select_Sql(string SQL, string[] parameter)
+        {
+            Boolean connection_result = DBConnection();
+            string[] result = new string[parameter.Length];
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(SQL, connection);
+                MySqlDataReader table = cmd.ExecuteReader();
+                //table_count = table.Cast<Object>().Count();
+                //result = new string[parameter.Length, table_count];
+
+                while (table.Read())
+                {
+                    for (int i = 0; i < parameter.Length; i++)
+                        result[i] = Convert.ToString(table[parameter[i]]);
+                }
+                return result;
+            }
+            catch (Exception ex) // If SQL process failed
+            {
+                connection.Close();
+            }
+
+            return result;
+        }
+
+        // 상황에 따라 SQL 호출
+        internal String Get_IP()
+        {
+            return Server_IP;
+        }
+
+        internal String Get_Port()
+        {
+            return Server_Port;
+        }
+
+        internal String Get_Name()
+        {
+            return DB_Name;
+        }
+
+        internal String Get_User()
+        {
+            return DB_User;
+        }
+
+        internal String Get_Password()
+        {
+            return DB_Password;
+        }
+    }
+}
